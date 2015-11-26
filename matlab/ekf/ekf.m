32a93412989f6a629bb78117c5435dc55983a1f1
lzy@t430 <ziyang.d.lee@gmail.com>
2015-11-26 15:41:18 +0800

    added ukf files

--- matlab/ekf/ekf.m
+++ matlab/ekf/ekf.m
@@ -17,37 +17,37 @@ function [x,P] = ekf(fstate, x, P, hmeas, z, Q, R)
 %           P: "a posteriori" state covariance
 %
 % Example:
-%{
-n  = 3;                                          % number of state
-q  = 0.1;                                        % std of process 
-r  = 0.1;                                        % std of measurement
-Q  = q^2*eye(n);                                 % covariance of process
-R  = r^2;                                        % covariance of measurement  
-f  = @(x)[x(2); x(3); 0.05*x(1)*(x(2)+x(3))];    % nonlinear state equations
-h  = @(x)x(1);                                   % measurement equation
-s  = [0; 0; 1];                                  % initial state
-x  = s+q*randn(3, 1);                            % initial state with noise
-P  = eye(n);                                     % initial state covraiance
-N  = 20;                                         % total dynamic steps
-
-xV = zeros(n, N);                                % estimate
-sV = zeros(n, N);                                % actual
-zV = zeros(1, N);                                % measurement
-
-for k = 1 : N
-  z       = h(s) + r*randn;                      % measurments with noise
-  sV(:,k) = s;                                   % save actual state
-  zV(k)   = z;                                   % save measurment
-  [x, P]  = ekf(f, x, P, h, z, Q, R);            % ekf 
-  xV(:,k) = x;                                   % save estimate
-  s = f(s) + q*randn(3,1);                       % update process 
-end
-
-for k = 1:3                                      % plot results
-  subplot(3, 1, k)
-  plot(1:N, sV(k,:), '-', 1:N, xV(k,:), '--')
-end
-%}
+%
+% n  = 3;                                          % number of state
+% q  = 0.1;                                        % std of process 
+% r  = 0.1;                                        % std of measurement
+% Q  = q^2*eye(n);                                 % covariance of process
+% R  = r^2;                                        % covariance of measurement  
+% f  = @(x)[x(2); x(3); 0.05*x(1)*(x(2)+x(3))];    % nonlinear state equations
+% h  = @(x)x(1);                                   % measurement equation
+% s  = [0; 0; 1];                                  % initial state
+% x  = s+q*randn(3, 1);                            % initial state with noise
+% P  = eye(n);                                     % initial state covraiance
+% N  = 20;                                         % total dynamic steps
+%
+% xV = zeros(n, N);                                % estimate
+% sV = zeros(n, N);                                % actual
+% zV = zeros(1, N);                                % measurement
+%
+% for k = 1 : N
+%   z       = h(s) + r*randn;                      % measurments with noise
+%   sV(:,k) = s;                                   % save actual state
+%   zV(k)   = z;                                   % save measurment
+%   [x, P]  = ekf(f, x, P, h, z, Q, R);            % ekf 
+%   xV(:,k) = x;                                   % save estimate
+%   s = f(s) + q*randn(3,1);                       % update process 
+% end
+%
+% for k = 1:3                                      % plot results
+%   subplot(3, 1, k)
+%   plot(1:N, sV(k,:), '-', 1:N, xV(k,:), '--')
+% end
+%
 % By Yi Cao at Cranfield University, 02/01/2008
 %
 
